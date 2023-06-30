1a5c5d62308a8939bfc1658fb6657867492ee4ab
Phillip Lloyd <phillip.lloyd@student.moringaschool.com>
2023-06-30 06:41:18 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution in this file!
+function distanceFromHqInFeet(someValue) {
+    distanceFromHqInBlocks(someValue);
+    // call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function,
+    // passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+
+    // the return value of distanceFromHqInBlocks can then be used to calculate feet
+}
